bca14390ab42c66f7400629b3fb33741c6a85112
Brandon <brandon.owen@hotmail.com>
2018-11-07 09:53:08 +1100

    Update latency

--- latency.go
+++ latency.go
@@ -25,11 +25,23 @@ func GlonassTime(e uint32) time.Duration {
     return now.Sub(sow.AddDate(0, 0, dow).Add(tod).Add(-(3 * time.Hour)))
 }
 
+func GlonassTimeShort(e uint32) time.Duration {
+    now := time.Now().UTC()
+    dow := now.Truncate(time.Hour * 24)
+    tod := time.Duration(e & 0x7FFFFFF) * time.Millisecond
+    return now.Sub(dow.Add(tod).Add(-(3 * time.Hour)))
+}
+
 func main() {
     c, _ := url.Parse(os.Args[1])
     r, _ := ntrip.Client(c, os.Args[2], os.Args[3])
 
     e := rtcm.Scan(r, func(msg rtcm.Rtcm3Message) {
+        //if msg.Number() == uint16(1020) {
+        //    j, _ := json.Marshal(msg)
+        //    fmt.Println(string(j))
+        //}
+        //return
         switch int(msg.Number()) {
             case 1077, 1097, 1117:
                 fmt.Println(msg.Number(), GpsTime(msg.(*rtcm.Rtcm3MessageMsm7).Header.Epoch))
@@ -45,6 +57,14 @@ func main() {
                 fmt.Println(msg.Number(), GpsTime(msg.(*rtcm.Rtcm3Message1003).Header.Epoch))
             case 1004:
                 fmt.Println(msg.Number(), GpsTime(msg.(*rtcm.Rtcm3Message1004).Header.Epoch))
+            case 1009:
+                fmt.Println(msg.Number(), GlonassTimeShort(msg.(*rtcm.Rtcm3Message1009).Header.Epoch))
+            case 1010:
+                fmt.Println(msg.Number(), GlonassTimeShort(msg.(*rtcm.Rtcm3Message1010).Header.Epoch))
+            case 1011:
+                fmt.Println(msg.Number(), GlonassTimeShort(msg.(*rtcm.Rtcm3Message1011).Header.Epoch))
+            case 1012:
+                fmt.Println(msg.Number(), GlonassTimeShort(msg.(*rtcm.Rtcm3Message1012).Header.Epoch))
         }
     })
     fmt.Println(e)
