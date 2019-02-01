f0897233d94d4c0e8ea7ba9be5f6adaa544e196d
Jonathan Mettes <jonathan@jmettes.com>
2019-02-01 21:03:26 +1100

    Add frame preamble test

--- pkgs/rtcm3/rtcm3_test.go
+++ pkgs/rtcm3/rtcm3_test.go
@@ -12,9 +12,13 @@ import (
 
 func TestMessageUnknown(t *testing.T) {
     messageInvalidNumber := data.Message1001
-    messageInvalidNumber.GpsObservationHeader.MessageNumber = 0
+    messageInvalidNumber.GpsObservationHeader.MessageNumber = 1018  // doesn't exist, as far as I know
 
     unknownMessage := rtcm3.DeserializeMessage(messageInvalidNumber.Serialize())    
+
+    if unknownMessage.Number() != 1018 {
+       t.Errorf("MessageUnknown message number incorrect")
+    }
 
     if reflect.TypeOf(unknownMessage).Name() != "MessageUnknown" {
         t.Errorf("Deserialize incorrectly infers message type - should default to unknown")
@@ -25,9 +29,20 @@ func TestMessageUnknown(t *testing.T) {
     }
 }
 
+func TestInvalidPreamble(t *testing.T) {
+    frameInvalidPreamble := rtcm3.EncapsulateMessage(data.Message1001)
+    frameInvalidPreamble.Preamble = 0xd2  // should be 0xd3
+
+    frameInvalidPreambleReader := bufio.NewReader(bytes.NewReader(frameInvalidPreamble.Serialize()))
+    _, err := rtcm3.DeserializeFrame(frameInvalidPreambleReader)
+    if err == nil || err.Error() != "Invalid Preamble" {
+        t.Errorf("Did not catch invalid Preamble")
+    }
+}
+
 func TestInvalidCrc(t *testing.T) {
     frameInvalidCrc := rtcm3.EncapsulateMessage(data.Message1001)
-    frameInvalidCrc.Crc = 0  // should be 0xb3dfd0
+    frameInvalidCrc.Crc = 0xb3dfc9  // should be 0xb3dfd0
 
     frameInvalidCrcReader := bufio.NewReader(bytes.NewReader(frameInvalidCrc.Serialize()))
     _, err := rtcm3.DeserializeFrame(frameInvalidCrcReader)
