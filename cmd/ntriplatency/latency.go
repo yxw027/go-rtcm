0071e71d71b6d015f9e34918d8076bce928e9b5b
Brandon <brandon.owen@hotmail.com>
2019-02-28 20:50:20 +1100

    Update ntriplatency to use new version of ntrip.Client

--- cmd/ntriplatency/latency.go
+++ cmd/ntriplatency/latency.go
@@ -2,15 +2,26 @@ package main
 
 import (
     "github.com/geoscienceaustralia/go-rtcm/rtcm3"
-    "github.com/umeat/go-ntrip/pkgs/ntrip"
+    "github.com/umeat/go-ntrip/ntrip"
     "fmt"
-    "os"
     "time"
+    "flag"
 )
 
 func main() {
-    r, _ := ntrip.Client(os.Args[1], os.Args[2], os.Args[3])
-    scanner := rtcm3.NewScanner(r)
+    caster := flag.String("caster", "http://auscors.ga.gov.au:2101/ALIC7", "NTRIP caster mountpoint to stream from")
+    username := flag.String("username", "", "NTRIP username")
+    password := flag.String("password", "", "NTRIP password")
+    flag.Parse()
+
+    client, err := ntrip.NewClient(*caster)
+    client.SetBasicAuth(*username, *password)
+    resp, err := client.Connect()
+    if err != nil || resp.StatusCode != 200 {
+        fmt.Println(resp.StatusCode, err)
+    }
+
+    scanner := rtcm3.NewScanner(resp.Body)
     for msg, err := scanner.Next(); err == nil; msg, err = scanner.Next() {
         switch int(msg.Number()) {
             case 1071, 1072, 1073, 1074, 1075, 1076, 1077,
--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/geoscienceaustralia/go-rtcm
 
 require (
 	github.com/bamiaux/iobit v0.0.0-20170418073505-498159a04883
-	github.com/benburkert/http v0.0.0-20140610191404-7b1c7d0a482c // indirect
 	github.com/google/go-cmp v0.2.0
-	github.com/umeat/go-ntrip v0.0.0-20190218042343-e07f06f966af
+	github.com/umeat/go-ntrip v0.0.0-20190228093706-fa7f05471fcc
 )
