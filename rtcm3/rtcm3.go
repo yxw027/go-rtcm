4dc63ecebbb29aab5e44faff01617a92c8cd3cf8
Brandon <brandon.owen@hotmail.com>
2019-06-13 20:50:24 +1000

    Add NextFrame method

--- rtcm3/rtcm3.go
+++ rtcm3/rtcm3.go
@@ -257,14 +257,23 @@ func NewScanner(r io.Reader) Scanner {
 }
 
 func (scanner Scanner) Next() (message Message, err error) {
+	frame, err := scanner.NextFrame()
+	if err != nil {
+		return nil, err
+	}
+	return DeserializeMessage(frame.Payload), err // probably have DeserializeMessage return err
+}
+
+func (scanner Scanner) NextFrame() (frame Frame, err error) {
 	for {
 		frame, err := DeserializeFrame(scanner.Reader)
 		if err != nil {
 			if err.Error() == "Invalid Preamble" || err.Error() == "CRC Failed" {
+				// Continue reading from next byte if a valid Frame was not found
+				//TODO: return byte array of skipped bytes
 				continue
 			}
-			return nil, err
 		}
-		return DeserializeMessage(frame.Payload), err // probably have frame.Message() return err
+		return frame, err
 	}
 }
